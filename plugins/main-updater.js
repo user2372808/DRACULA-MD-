f9a41fcd45fd46cca63d4213f92b37bf1c84817e
PHAROUK56 <mariam45sidibe45@gmail.com>
2025-04-16 16:58:14 +0000

    Update main-updater.js

--- plugins/main-updater.js
+++ plugins/main-updater.js
@@ -19,7 +19,7 @@ cmd({
         await reply("🔍 Checking for KHAN-MD updates...");
 
         // Fetch the latest commit hash from GitHub
-        const { data: commitData } = await axios.get("https://api.github.com/repos/XdTechPro/KHAN-MD/commits/main");
+        const { data: commitData } = await axios.get("https://api.github.com/repos/PHAROUK56/DRACULA-MD/commits/main");
         const latestCommitHash = commitData.sha;
 
         // Get the stored commit hash from the database
@@ -33,7 +33,7 @@ cmd({
 
         // Download the latest code
         const zipPath = path.join(__dirname, "latest.zip");
-        const { data: zipData } = await axios.get("https://github.com/DRACULA-MD/DRACULA-MD/archive/main.zip", { responseType: "arraybuffer" });
+        const { data: zipData } = await axios.get("https://github.com/PHAROUK56/DRACULA-MD-/archive/main.zip", { responseType: "arraybuffer" });
         fs.writeFileSync(zipPath, zipData);
 
         // Extract ZIP file
