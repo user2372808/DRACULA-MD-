e1790a12d15059c381f49d5cef1e0e7a86b2f073
PHAROUK56 <mariam45sidibe45@gmail.com>
2025-04-23 12:01:52 +0000

    Create config.js

---
+++ config.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || "KHAN-MD~BbMBkAIB#Y0K5hHrEj8lqG7RmTl0ADJ6UreYwKmeSgSo5QAzWAOc",
+AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
+AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
+AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
+AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*VUE PAR PHAROUK 🟢😝",
+ANTI_LINK: process.env.ANTI_LINK || "true",
+MENTION_REPLY: process.env.MENTION_REPLY || "false",
+MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/sauy49.jpg",
+WELCOME: process.env.WELCOME || "true",
+PREFIX: process.env.PREFIX || ".",
+BOT_NAME: process.env.BOT_NAME || "DRACULA-MD",
+STICKER_NAME: process.env.STICKER_NAME || "DRACULA-MD",
+CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
+CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍",
+DELETE_LINKS: process.env.DELETE_LINKS || "false",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "22542758XXXX",
+OWNER_NAME: process.env.OWNER_NAME || "PHAROUK",
+DESCRIPTION: process.env.DESCRIPTION || "SIMPLE WHATSAPP BOT CRÉÉ PAR PHAROUK",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/fooqjk.jpg",
+LIVE_MSG: process.env.LIVE_MSG || "> SALUT ",
+READ_MESSAGE: process.env.READ_MESSAGE || "false",
+AUTO_REACT: process.env.AUTO_REACT || "false",
+ANTI_BAD: process.env.ANTI_BAD || "false",
+MODE: process.env.MODE || "public",
+ANTI_LINK_KICK: process.env.ANTI_LINK_KICK || "false",
+AUTO_VOICE: process.env.AUTO_VOICE || "false",
+AUTO_STICKER: process.env.AUTO_STICKER || "false",
+AUTO_REPLY: process.env.AUTO_REPLY || "false",
+ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
+PUBLIC_MODE: process.env.PUBLIC_MODE || "true",
+AUTO_TYPING: process.env.AUTO_TYPING || "false",
+READ_CMD: process.env.READ_CMD || "false",
+DEV: process.env.DEV || "2250575130788",
+ANTI_VV: process.env.ANTI_VV || "true",
+ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "log",
+AUTO_RECORDING: process.env.AUTO_RECORDING || "false"
+};
